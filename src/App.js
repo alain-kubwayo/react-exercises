11a5ed6e450c41a0f09515fedb608daf6976f93d
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-20 18:05:25 +0200

    feat: add navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import HelloWorld from "./components/HelloWorld";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/hello-world" element={<HelloWorld />} />
       </Routes>
