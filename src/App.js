1c211238ff6c687ae1434945551c4a4e84fe1199
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-20 19:07:58 +0200

    feat: add counter component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Navbar from "./components/Navbar";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={(<h1>Test</h1>)} />
+        <Route path="/counter" element={(<Counter />)} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return ( 
+        <nav className="flex w-full px-10 py-4 border-b-2 justify-evenly border-sky-900">
+            <Link to="/states-and-props" className="text-xl text-sky-900">Exercise 4</Link>
+        </nav>
+    );
+}
+
+export default Navbar;
