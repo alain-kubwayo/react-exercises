7077df5a3b7c4e1181c1622fb395b611edfd7dce
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-20 20:56:56 +0200

    feat: add jokes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Jokes from "./components/Jokes";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={(<h1>Test</h1>)} />
+        <Route path="/" element={(<h1 className="flex items-center justify-center h-[50vh] text-2xl font-bold text-sky-900 uppercase">Welcome, click the link to visit the exercise</h1>)} />
+        <Route path="/jokes" element={<Jokes />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Jokes.jsx
@@ -0,0 +1,16 @@
+import Joke from "./Joke";
+import jokes from "../shareable/jokes";
+import { useState } from "react";
+
+const Jokes = () => {
+    const [allJokes, setAllJokes] = useState(jokes);
+    console.log(allJokes);
+
+    return ( 
+        <div className="grid max-w-2xl grid-cols-1 gap-20 pt-10 mx-auto md:grid-cols-2">
+            {allJokes.map(joke => <Joke {...joke} key={joke.id} />)}
+        </div>
+    );
+}
+ 
+export default Jokes;
