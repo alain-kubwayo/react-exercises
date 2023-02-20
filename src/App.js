f51cbb92fd46c9198bb73bab881e5dbce1df6e87
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-20 20:34:59 +0200

    feat: add form for greeting user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import Form from "./components/Form";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={(<h1>Test</h1>)} />
+        <Route path="/form" element={(<Form />)} />
       </Routes>
     </BrowserRouter>
   );
