6fc642f5f3cc8e757f3375babb9a7bbb428299f8
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-20 20:05:27 +0200

    feat: add list of animals using custom component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import AnimalsList from "./components/AnimalsList";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={(<h1>Test</h1>)} />
+        <Route path="/animal-list" element={<AnimalsList />} />
       </Routes>
     </BrowserRouter>
   );
