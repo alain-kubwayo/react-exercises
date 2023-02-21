57ad8af390982e7218fbe53dd11a90439a541615
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-21 11:41:32 +0200

    refactor: remove unnecessary log

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 
 const Jokes = () => {
     const [allJokes, setAllJokes] = useState(jokes);
-    console.log(allJokes);
 
     return ( 
         <div className="grid max-w-2xl grid-cols-1 gap-20 pt-10 mx-auto md:grid-cols-2">
