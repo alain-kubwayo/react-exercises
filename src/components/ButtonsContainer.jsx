ac1f4955fcb4209d98c5604be565b3b0d0e662cf
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-21 14:28:57 +0200

    refactor: remove unnecessary useEffect

--- src/components/ButtonsContainer.jsx
+++ src/components/ButtonsContainer.jsx
@@ -3,12 +3,7 @@ import { useState, useEffect } from "react";
 import CustomButton from "./buttons/CustomButton";
 
 const ButtonsContainer = () => {
-    const [buttonsDetails, setButtonsDetails] = useState(null);
-    
-    useEffect(() => {
-        const initData = () => setButtonsDetails(buttonsData);
-        initData();
-    }, []);
+    const [buttonsDetails, setButtonsDetails] = useState(buttonsData);
 
     const handleButtonClick = (id) => {
         alert(`You clicked on Button ${id}`);
@@ -16,7 +11,7 @@ const ButtonsContainer = () => {
 
     return ( 
         <div className="flex justify-between w-full mx-auto mt-10 md:w-1/2">
-            {buttonsDetails && buttonsDetails.map(detail => (
+            {buttonsDetails?.map(detail => (
                 <CustomButton
                     {...detail}
                     handleButtonClick={handleButtonClick}
