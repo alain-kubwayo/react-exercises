a00132b6f18b9f543ed19f5c698b9dc09151cf6d
alain-kubwayo <ahkubwayo@gmail.com>
2023-02-22 19:45:47 +0200

    refactor: change animals variable from uppercase to lowercase

--- src/components/Animals.jsx
+++ src/components/Animals.jsx
@@ -1,10 +1,10 @@
-const ANIMALS = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'];
+const animals = ['dog', 'cat', 'chicken', 'cow', 'sheep', 'horse'];
 
 const Animals = () => {
     return ( 
         <div className="w-full pt-10 mx-auto md:w-1/2">
             <ul>
-                {ANIMALS.map((animal, index) => <li key={index}>{animal}</li>)}
+                {animals.map((animal, index) => <li key={index}>{animal}</li>)}
             </ul>
         </div>
      );
