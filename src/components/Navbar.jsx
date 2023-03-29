80902abc18a814bb97378e16228a88eed457b424
alain-kubwayo <ahkubwayo@gmail.com>
2023-03-29 19:00:15 +0200

    fix: resolve conflicts in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <nav className="flex w-full px-10 py-4 border-b-2 justify-evenly border-sky-900">
-<<<<<<< HEAD
             <Link to="/capture-user-clicks" className="text-xl text-sky-900">Exercise 2</Link>
-        </nav>
-    );
-}
- 
-=======
             <Link to="/custom-component" className="text-xl text-sky-900">Exercise 3</Link>
             <Link to="/counter" className="text-xl text-sky-900">Exercise 4</Link>
             <Link to="/imperative-animal-list" className="text-xl text-sky-900">Exercise 5</Link>
@@ -20,5 +14,4 @@ const Navbar = () => {
     );
 }
 
->>>>>>> main
 export default Navbar;
